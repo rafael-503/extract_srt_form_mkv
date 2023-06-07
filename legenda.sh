e364baf93e21a5ea63c673a13054cecb19570dd4
Rafael Bergo <rafaelbergo503@gmail.com>
2023-06-07 01:17:38 -0300

    corrigido bug ao converter a legenda de ass para srt

--- legenda.sh
+++ legenda.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
   echo "Usage: $0 <filename> [track=2] [format=srt]"
   exit 1
 fi
@@ -10,8 +9,7 @@ filename="$1"
 track="${2:-2}" # define a faixa de legenda como 2, se não especificado
 format="${3:-srt}"  # define o formato como srt, se nenhum formato for especificado
 
-if [[ ! -f "$filename" ]]
-then
+if [[ ! -f "$filename" ]]; then
   echo "Arquivo '$filename' não encontrado."
   exit 1
 fi
@@ -22,14 +20,13 @@ echo "Extraindo a legenda da faixa $track do arquivo '$filename'..."
 if mkvinfo "$filename" | grep -q "Codec ID: S_TEXT/ASS"; then
   echo "Arquivo de legenda é no formato ASS, convertendo para SRT..."
   mkvextract "$filename" tracks $track:"${filename%.*}.ass"
-  ass2srt "${filename%.*}.ass" -o "${filename%.*}.srt"
+  ffmpeg -i "${filename%.*}.ass" "${filename%.*}.srt"
   rm "${filename%.*}.ass"
 else
   mkvextract "$filename" tracks 2:"${filename%.*}.$format"
 fi
 
-if [[ ! -f "${filename%.*}.$format" ]]
-then
+if [[ ! -f "${filename%.*}.$format" ]]; then
   echo "Falha na extração da legenda."
   exit 1
 fi
